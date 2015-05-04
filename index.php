61f28a457fb38821dfa9c4dcde9d381aebc4839c
Gaston Briones <mauricio_briones@artear.com.ar>
2015-05-04 18:16:34 -0300

    commit

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <meta content="Arte pop, latinoamérica-Arte, pop, Argentina-Art pop, Argentina-Arte pop, Salta-Art pop, Salta-Pintura pop, Argentina-Pintura pop, Buenos Aires-Pintura pop, Salta-Cuadros pop, Argentina-Dibujos pop, Argentina-artista, plástico, Argentina-artista plástico, Buenos Aires-artista, plástico Salta-artista, visual, Argentina-artista visual, Buenos Aires-Artista visual, Salta-pop, drawing-pop, painting-Arte, contemporaneo, Argentina-Arte contemporaneo, Salta-Estilo, pop-Serie, Pop-Muestra, pop-Exposición, arte, pop-Mural, pop-Mural Buenos Aires-colores, saturados-dibujo, oriental-oriental, drawing-dibujo, zen-Dibujo, geisha-objetos, intervenidos, pop-Contraste-arte, mujer-arte, imagen mujer-arte, extravagante-arte, geisha" name="keywords">
+
+    <meta content="Descripcion" name="description">
+    <!-- Facebook -->
+    <meta name="og:type" content="website"/>
+    <meta name="og:title" content="Francisco Speicher"/>
+    <meta name="og:description" content="Descripcion"/>
+    <meta name="og:url" content="url"/>
+    <meta content="Francisco Speicher" name="og:site_name">
+    <meta content="Buenos Aires" name="og:region">
+    <meta content="Argentina" name="og:country-name">
+    <!-- twitter -->
+    <meta name="twitter:card" content="summary"/>
+    <meta name="twitter:url" content="url"/>
+    <meta name="twitter:title" content="Francisco Speicher"/>
+    <meta name="twitter:description" content="Descripcion"/>
+    <meta content="@franspeicher" name="twitter:site">
+    <meta content="@gasbriones" name="twitter:creator">
+    <!-- Fonts -->
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:300,600&amp;subset=latin,latin-ext,cyrillic-ext" type="text/css" media="all" />
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Ubuntu:400,300,300italic,400italic,500,700,500italic,700italic&subset=latin,cyrillic-ext,greek-ext,greek,latin-ext,cyrillic" type="text/css" media="all" />
+    <!-- Links -->
+    <link href="css/normalize.css" rel="stylesheet" type="text/css">
+    <link href="css/jquery.bxslider.css" rel="stylesheet" type="text/css">
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <link href="css/mediaqueries.css" rel="stylesheet" type="text/css">
+    <title>Service Page</title>
+
+</head>
+<body class="home" data-page="home">
+<div class="page">
+    <div class="header-container clearfix">
+        <header id="header">
+            <div class="nav">
+                <div class="wrapper">
+                    <nav>
+                        <ul id="header-links">
+                            <li>
+                                <a href="#"> Link 1</a>
+                            </li>
+                            <li>
+                                <a href="#"> Link 2</a>
+                            </li>
+                        </ul>
+                        <ul id="header-social">
+                            <li>
+                                <a href="#"> Link 1</a>
+                            </li>
+                            <li>
+                                <a href="#"> Link 2</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </header>
+    </div>
+</div>
+</body>
+</html>
